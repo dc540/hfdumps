83ee08a8d32cb3b4a35f7c0ba8e84ae00992884b
DC540 <dc540@dc540.org>
2020-10-19 11:28:21 -0400

    Added analysis document

--- readmyshit.pl
+++ readmyshit.pl
@@ -18,12 +18,16 @@ sub processCard {
   open CARD, "$thisCard" or die $!;
   $bh0=<CARD>;
   print "Card $cardNum Sector 0 header: \n  $bh0";
+  $isdata=0;
   $bh01=<CARD>;
   chomp;
   print "  $bh01";
   $bh02=<CARD>;
   chomp;
   print "  $bh02";
+  if (($bh01 != '00000000000000000000000000000000') or ($bh02 != '00000000000000000000000000000000')) {
+    $isdata=1;
+  };
   $y=0;
   for ($y=1; $y<16; $y++) {
     $header=<CARD>;
@@ -34,18 +38,28 @@ sub processCard {
     # print "$header";
     print "KEYS: (A)$keya : (B)$keyb (Access Bits)$accessBits";
     $data0=<CARD>;
+    chomp $data0;
+    $ascii0 = `echo "$data0" | xxd -r -p 2>&1`;
     $data1=<CARD>;
+    chomp $data1;
+    $ascii1 = `echo "$data1" | xxd -r -p 2>&1`;
     $data2=<CARD>;
+    chomp $data2;
+    $ascii2 = `echo "$data2" | xxd -r -p 2>&1`;
     if (($data0 != '00000000000000000000000000000000') or ($data1 != '00000000000000000000000000000000') or ($data2 != '00000000000000000000000000000000')) {
-      print "\n  " . $data0;
-      print "  " . $data1;
-      print "  " . $data2;
+      $isdata=1;
+      print "\n  " . $data0 . " (Asc: " . $ascii0 . ")\n";
+      print "  " . $data1 . " (Asc: " . $ascii1 . ")\n";
+      print "  " . $data2 . " (Asc: " . $ascii2 . ")\n";;
       print "";
     } else {
       print " (all zeroes, no data)\n";
     };
     #$thissector=$sector[$y];
     #$thisata=$data[$y];
+  };
+  if (!$isdata) {
+    print "######## $thisCard: NO DATA BEYOND SECTOR ZERO BLOCK ZERO!\n";
   };
   return;
 };
